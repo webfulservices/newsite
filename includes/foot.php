74fc3f82025062e3aa0efb1eea329deb04e808ab
webfulservices <65138828+webfulservices@users.noreply.github.com>
2020-05-20 01:44:44 +0100

    Update foot.php

--- includes/foot.php
+++ includes/foot.php
@@ -49,16 +49,16 @@
   <a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
 
   <!-- Vendor JS Files -->
-  <script src="vendor/jquery/jquery.min.js"></script>
-  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="vendor/jquery.easing/jquery.easing.min.js"></script>
-  <script src="vendor/php-email-form/validate.js"></script>
-  <script src="vendor/waypoints/jquery.waypoints.min.js"></script>
-  <script src="vendor/counterup/counterup.min.js"></script>
-  <script src="vendor/jquery-sticky/jquery.sticky.js"></script>
-  <script src="vendor/owl.carousel/owl.carousel.min.js"></script>
-  <script src="vendor/isotope-layout/isotope.pkgd.min.js"></script>
-  <script src="vendor/venobox/venobox.min.js"></script>
+  <script src="jquery/jquery.min.js"></script>
+  <script src="bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="jquery.easing/jquery.easing.min.js"></script>
+  <script src="php-email-form/validate.js"></script>
+  <script src="waypoints/jquery.waypoints.min.js"></script>
+  <script src="counterup/counterup.min.js"></script>
+  <script src="jquery-sticky/jquery.sticky.js"></script>
+  <script src="owl.carousel/owl.carousel.min.js"></script>
+  <script src="isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="venobox/venobox.min.js"></script>
 
   <!-- Template Main JS File -->
   <script src="js/main.js"></script>
